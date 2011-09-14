eb0de60040d1c0f76ebdc5abd35215bee353ed3f
Hellen Escarate <hellen.escarate@objectos.com.br>
2011-09-14 17:09:19 -0300

    adicionado duas novas classes TesteUnitario e Soma

---
+++ objectos-dojo-hescarate/testesunitarios/dojo1/TesteUnitario.java
@@ -0,0 +1,15 @@
+package dojo1;
+
+import dojo2.Soma; 
+
+public class TesteUnitario{
+  public static void main(String[] args){
+
+    Soma resultado = new Soma();
+    resultado.soma(1,2);      
+      
+    
+    
+  }   
+}
+    
